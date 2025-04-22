9a344eb9e6769076f2ee58cdd1dea9572dcd99d3
va2rwl <va2rwl@users.noreply.github.com>
2025-04-23 08:50:08 +0900

    マイグレーションファイルの-を_に訂正

--- database/migrations/2025_04_17_054117_create_daily_logs_table.php
+++ database/migrations/2025_04_17_054117_create_daily_logs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('daily-logs', function (Blueprint $table) {
+        Schema::create('daily_logs', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->date('log_date');
@@ -38,6 +38,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('daily-logs');
+        Schema::dropIfExists('daily_logs');
     }
 };
